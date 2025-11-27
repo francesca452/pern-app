9c5539c66378278571bc128ebf263fa0e14eed28
francescamazz <mazzetti.francy01@gmail.com>
2025-11-27 16:59:14 +0100

    correction to the spots.js file

---
+++ backend/seeds/spots.js
@@ -0,0 +1,37 @@
+import { sql } from "../config/db.js";
+
+const SAMPLE_SPOTS = [
+    {
+        street: "Via Zamboni 33",
+        tot_available: 30,
+    },
+    {
+        street: "Via Antonio 9/3a",
+        tot_available: 15,
+    },
+];
+
+async function seedDatabase() {
+    try {
+        // First, clear existing data
+        await sql.query(`TRUNCATE TABLE parkingspots RESTART IDENTITY`);
+
+        // Insert all spots
+        for (const spot of SAMPLE_SPOTS) {
+            await sql.query(`
+                INSERT INTO parkingspots (street, tot_available)
+                VALUES ($1, $2)`,
+            [spot.street, spot.tot_available]
+        );
+        }
+
+        console.log("Database seeded successfully");
+        process.exit(0); // Success code
+
+    } catch (error) {
+        console.error("Error seeding database:", error);
+        process.exit(1); // Failed code
+    }
+}
+
+seedDatabase();
